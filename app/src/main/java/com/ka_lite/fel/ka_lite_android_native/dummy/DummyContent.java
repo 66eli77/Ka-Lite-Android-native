2b3a21865b4cc7eb162f173a9b4f93f9710914e9
Aron Fyodor Asor <aronasorman@gmail.com>
2014-10-04 16:28:36 -0700

    clean up DummyContent

--- app/src/main/java/com/ka_lite/fel/ka_lite_android_native/dummy/DummyContent.java
+++ app/src/main/java/com/ka_lite/fel/ka_lite_android_native/dummy/DummyContent.java
@@ -15,7 +15,6 @@ import android.os.Environment;
  */
 public class DummyContent {
     private File root;
-    private ArrayList<File> fileList = new ArrayList<File>();
     /**
      * An array of sample (dummy) items.
      */
@@ -28,75 +27,28 @@ public class DummyContent {
 
     //constructor
     public DummyContent() {
-        //getting SDcard root path
+        populateItemMap();
+
+    }
+
+    private void populateItemMap() {
+        // getting SDcard root path
         root = new File(Environment.getExternalStorageDirectory()
                 .getAbsolutePath());
-        getfile(root);
 
-        for (int i = 0; i < fileList.size(); i++) {
-
-            if (fileList.get(i).isDirectory()) {
-                DummyItem item = new DummyItem(i + "", "folder " + fileList.get(i).getName(), fileList.get(i).getPath());
+        for (File f : root.listFiles()) {
+            if (f.isDirectory()) {
+                DummyItem item = new DummyItem("", "folder " + f.getName(), f.getPath());
                 ITEMS.add(item);
                 ITEM_MAP.put(item.id, item);
-            }else{
+            } else {
                 //addItem(new DummyItem(i + "", fileList.get(i).getName()));
-                DummyItem item = new DummyItem(i + "", fileList.get(i).getName(), fileList.get(i).getPath());
+                DummyItem item = new DummyItem("", f.getName(), f.getPath());
                 ITEMS.add(item);
                 ITEM_MAP.put(item.id, item);
             }
         }
     }
-
-    private boolean found = false;
-    private String CONTENTLOAD_ROOT = "CONTENTLOAD_ROOT";
-    public ArrayList<File> getfile(File dir) {
-        File listFile[] = dir.listFiles();
-        if (listFile != null && listFile.length > 0) {
-            for (int i = 0; i < listFile.length; i++) {
-
-                if (listFile[i].isDirectory()) {
-                    //    fileList.add(listFile[i]);
-                    //getfile(listFile[i]);
-                    if(!listFile[i].getName().equals(CONTENTLOAD_ROOT)) {
-                        System.out.println("nono found");
-                        getfile(listFile[i]);
-                        //found = true;
-                    }else{
-                        System.out.println("elieli found");
-                        File targetFile[] = listFile[i].listFiles();
-                        for(int j =0; j < targetFile.length; j++){
-                            fileList.add(targetFile[j]);
-                        }
-                    }
-                }
-
-//              } else {
-//                    if (listFile[i].getName().endsWith(".png")
-//                            || listFile[i].getName().endsWith(".jpg")
-//                            || listFile[i].getName().endsWith(".jpeg")
-//                            || listFile[i].getName().endsWith(".gif"))
-//
-//                    {
-//                        fileList.add(listFile[i]);
-//                    }
-
-            }
-        }
-        return fileList;
-    }
-
-//    static {
-//        // Add 3 sample items.
-//        addItem(new DummyItem("1", "Item 1"));
-//        addItem(new DummyItem("2", "Item 2"));
-//        addItem(new DummyItem("3", "Item 3"));
-//    }
-//
-//    public static void addItem(DummyItem item) {
-//        ITEMS.add(item);
-//        ITEM_MAP.put(item.id, item);
-//    }
 
     /**
      * A dummy item representing a piece of content.
